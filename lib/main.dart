bda888a3eb805b7870286bdf14b17d1b00b8b8b6
SivaSundaraNarayanan <sivasundaranarayanan@gmail.com>
2022-12-13 23:30:11 +0530

    Configured firebase for android app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:spoorfy/firebase_options.dart';
 
 void main() {
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
